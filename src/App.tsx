70d0f1cb8519fd1f43d79584766c041d82bd4e3e
luckyzkrazin96 <lakshankanishka0@gmail.com>
2024-12-03 05:04:55 +0530

    load stocks

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Category from './pages/Category'
 import Item from './pages/Item'
+import Stock from './pages/Stock'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/category" element={<Category />} />
         <Route path="/item" element={<Item />} />
+        <Route path="/stock" element={<Stock/>}/>
       </Routes>
     </BrowserRouter>
   )
--- src/pages/Stock.tsx
+++ src/pages/Stock.tsx
@@ -1,3 +1,56 @@
-function Stock(){
-    
+import { useEffect, useState } from "react";
+import StockType from "../types/StockType";
+import ItemType from "../types/ItemType";
+import axios from "axios";
+
+function Stock() {
+
+    const [stocks, setStocks] = useState<StockType[]>([])
+    const [items, setItems] = useState<ItemType[]>([])
+
+    async function loadStock() {
+        const response = await axios.get("http://localhost:8080/stocks")
+        setStocks(response.data)
+    }
+
+    useEffect(function () {
+        loadStock();
+    }, [])
+
+    return (
+        <div className="container mx-auto pt-5 pb-5">
+            <h1 className="text-3xl font-semibold mb-5">Stocks</h1>
+            <table className="w-full border-separate border-spacing-0 border-none text-left">
+                <thead className="bg-slate-200">
+                    <tr>
+                        <th className="w-[80px]">Stock Id</th>
+                        <th className="w-[200px]">Item</th>
+                        <th className="w-[80px]">Quntity</th>
+                        <th className="w-[200px]">Actions</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {stocks.map(function (stock) {
+                        return (
+                            <tr>
+                                <td>{stock.id}</td>
+                                <td>{stock.item.name}</td>
+                                <td>{stock.qty}</td>
+                                <td>
+                                    <button className="bg-slate-200 text-slate-600 px-2 py-1 rounded-lg hover:bg-slate-300">Edit</button>
+                                    <button className="bg-red-400 text-white rounded-lg px-2 py-1 hover:bg-red-500">Delete</button>
+                                </td>
+                            </tr>
+
+
+                        )
+                    })
+
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
 }
+export default Stock;
